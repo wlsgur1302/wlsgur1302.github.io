3aadfb1ebc649f19c4c24222f0a4aebc9695fe9a
Hanga <108642241+wlsgur1302@users.noreply.github.com>
2023-10-10 13:01:59 +0900

    전체적인 내용 수정
    전체적인 내용 수정

---
+++ wlsgur1302.github.io/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        navigation: false,
+        navigationTooltips: ['INTRO', 'ABOUT', 'STACKS', 'WORKS','CONTACT'],
+        showActiveTooltip: true,
+        
+    
+    
+    });
+});
+
